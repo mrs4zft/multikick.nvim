0f02072ab6ce7f8495e8a811df01eba07f2aba94
mrs4zft <m.scholz@telematik-zentrum.de>
2025-01-15 11:13:15 +0100

    Adds neogit and diffview. Keeping fugitive for now.

---
+++ lua/user/neogit.lua
@@ -0,0 +1,27 @@
+local M = {
+	"neogitorg/neogit",
+	event = "VeryLazy",
+}
+
+function M.config()
+	vim.keymap.set("n", "<leader>gg", "<cmd>Neogit<CR>", { desc = "Neogit" })
+
+	require("neogit").setup({
+		auto_refresh = true,
+		disable_builtin_notifications = false,
+		use_magit_keybindings = false,
+		-- Change the default way of opening neogit
+		kind = "tab",
+		-- Change the default way of opening the commit popup
+		commit_popup = {
+			kind = "split",
+		},
+		-- Change the default way of opening popups
+		popup = {
+			kind = "split",
+		},
+		-- customize displayed signs
+	})
+end
+
+return M
